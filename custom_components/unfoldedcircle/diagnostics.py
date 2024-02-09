35be7ad107c44e66072633585418a3166e1f0fa4
Jack Powell <jackattack51@gmail.com>
2024-02-09 14:43:24 +0000

    Add diagnostic support

---
+++ custom_components/unfoldedcircle/diagnostics.py
@@ -0,0 +1,27 @@
+"""Diagnostics support for Unfolded Circle."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.components.diagnostics.util import async_redact_data
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN, UNFOLDED_CIRCLE_COORDINATOR
+from .coordinator import UnfoldedCircleRemoteCoordinator
+
+TO_REDACT = {
+    "apikey",
+    "pin",
+}
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    coordinator: UnfoldedCircleRemoteCoordinator = hass.data[DOMAIN][entry.entry_id][
+        UNFOLDED_CIRCLE_COORDINATOR
+    ]
+    return async_redact_data(coordinator.data, TO_REDACT)
