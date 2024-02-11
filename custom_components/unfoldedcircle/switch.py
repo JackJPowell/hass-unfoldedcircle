f21eaf0afd8e1f376570510e35f1394fa6fdcf2b
Jack Powell <jackattack51@gmail.com>
2024-02-11 23:13:31 +0000

    Add support for config switches

--- custom_components/unfoldedcircle/switch.py
+++ custom_components/unfoldedcircle/switch.py
@@ -1,19 +1,100 @@
 """Platform for Switch integration."""
 
 import logging
+from collections.abc import Callable
+from dataclasses import dataclass
+from typing import cast
 
-from homeassistant.components.switch import SwitchEntity
+from homeassistant.components.switch import (
+    SwitchDeviceClass,
+    SwitchEntity,
+    SwitchEntityDescription,
+)
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import DiscoveryInfoType
+from homeassistant.helpers.typing import StateType
 
 from .const import DOMAIN, UNFOLDED_CIRCLE_COORDINATOR
+from .coordinator import UnfoldedCircleRemoteCoordinator
 from .entity import UnfoldedCircleEntity
 from .pyUnfoldedCircleRemote.const import RemoteUpdateType
 
 _LOGGER = logging.getLogger(__name__)
+
+
+@dataclass
+class UnfoldedCircleSwitchEntityDescription(SwitchEntityDescription):
+    """Class describing Unfolded Circle Remote switch entities."""
+
+    unique_id: str = ""
+    control_fn: Callable = None
+
+
+def update_remote_display_settings(
+    coordinator: UnfoldedCircleRemoteCoordinator, enable: bool
+) -> None:
+    coordinator.api.patch_remote_display_settings(enable)
+
+
+def update_remote_button_settings(
+    coordinator: UnfoldedCircleRemoteCoordinator, enable: bool
+) -> None:
+    coordinator.api.patch_remote_button_settings(enable)
+
+
+def update_remote_sound_settings(
+    coordinator: UnfoldedCircleRemoteCoordinator, enable: bool
+) -> None:
+    coordinator.api.patch_remote_sound_settings(enable)
+
+
+def update_remote_haptic_settings(
+    coordinator: UnfoldedCircleRemoteCoordinator, enable: bool
+) -> None:
+    coordinator.api.patch_remote_haptic_settings(enable)
+
+
+UNFOLDED_CIRCLE_SWITCH: tuple[UnfoldedCircleSwitchEntityDescription, ...] = (
+    UnfoldedCircleSwitchEntityDescription(
+        key="display_auto_brightness",
+        device_class=SwitchDeviceClass.SWITCH,
+        entity_category=EntityCategory.CONFIG,
+        name="Display Auto Brightness",
+        unique_id="display_auto_brightness",
+        icon="mdi:brightness-auto",
+        control_fn=update_remote_display_settings,
+    ),
+    UnfoldedCircleSwitchEntityDescription(
+        key="button_backlight",
+        device_class=SwitchDeviceClass.SWITCH,
+        entity_category=EntityCategory.CONFIG,
+        name="Button Backlight",
+        unique_id="button_backlight",
+        icon="mdi:keyboard-settings",
+        control_fn=update_remote_button_settings,
+    ),
+    UnfoldedCircleSwitchEntityDescription(
+        key="sound_effects",
+        device_class=SwitchDeviceClass.SWITCH,
+        entity_category=EntityCategory.CONFIG,
+        name="Sound Effects",
+        unique_id="sound_effects",
+        icon="mdi:music",
+        control_fn=update_remote_sound_settings,
+    ),
+    UnfoldedCircleSwitchEntityDescription(
+        key="haptic_feedback",
+        device_class=SwitchDeviceClass.SWITCH,
+        entity_category=EntityCategory.CONFIG,
+        name="Haptic Feedback",
+        unique_id="haptic_feedback",
+        icon="mdi:vibrate",
+        control_fn=update_remote_haptic_settings,
+    ),
+)
 
 
 async def async_setup_entry(
@@ -35,6 +116,11 @@ async def async_setup_entry(
         for switch in filter(lambda a: a not in activities, coordinator.api.activities)
     )
 
+    async_add_entities(
+        UCRemoteConfigSwitch(coordinator, configSwitch)
+        for configSwitch in UNFOLDED_CIRCLE_SWITCH
+    )
+
 
 class UCRemoteSwitch(UnfoldedCircleEntity, SwitchEntity):
     """Class representing an unfolded circle activity."""
@@ -47,7 +133,7 @@ class UCRemoteSwitch(UnfoldedCircleEntity, SwitchEntity):
         # self._attr_unique_id = switch._id
         self._state = switch.state
         self._attr_icon = "mdi:remote-tv"
-        self._attr_native_value = "OFF"
+        self._state: StateType = None
 
     async def async_added_to_hass(self):
         """Run when this Entity has been added to HA."""
@@ -80,4 +166,62 @@ class UCRemoteSwitch(UnfoldedCircleEntity, SwitchEntity):
         except (KeyError, IndexError):
             return
         self._state = self.switch.state
+        self.async_write_ha_state()
+
+
+class UCRemoteConfigSwitch(UnfoldedCircleEntity, SwitchEntity):
+    """Class representing an unfolded circle activity."""
+
+    entity_description = UNFOLDED_CIRCLE_SWITCH
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return the device info."""
+        return DeviceInfo(
+            identifiers={
+                # Serial numbers are unique identifiers within a specific domain
+                (DOMAIN, self.coordinator.api.serial_number)
+            },
+            name=self.coordinator.api.name,
+            manufacturer=self.coordinator.api.manufacturer,
+            model=self.coordinator.api.model_name,
+            sw_version=self.coordinator.api.sw_version,
+            hw_version=self.coordinator.api.hw_revision,
+            configuration_url=self.coordinator.api.configuration_url,
+        )
+
+    def __init__(
+        self, coordinator, description: UnfoldedCircleSwitchEntityDescription
+    ) -> None:
+        """Initialize a switch."""
+        super().__init__(self, coordinator)
+        self._description = description
+        self.coordinator = coordinator
+        self.entity_description = description
+        self._attr_unique_id = (
+            f"{self.coordinator.api.serial_number}_{description.unique_id}"
+        )
+        self._attr_name = f"{self.coordinator.api.name} {description.name}"
+        key = "_" + self._description.key
+        self._state = coordinator.data.get(key)
+
+    @property
+    def is_on(self) -> bool | None:
+        """Return true if switch is on."""
+        return self._state is True
+
+    async def async_turn_on(self, **kwargs) -> None:
+        """Instruct the switch to turn on."""
+        self.entity_description.control_fn(True)
+
+    async def async_turn_off(self, **kwargs) -> None:
+        """Instruct the switch to turn off."""
+        self.entity_description.control_fn(False)
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        key = "_" + self._description.key
+        state = self.coordinator.data.get(key)
+        self._state = cast(StateType, state)
         self.async_write_ha_state()
