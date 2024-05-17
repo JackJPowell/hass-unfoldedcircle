9be3da490edfb540c085f0cb901cc0ed18c3bf78
Jack Powell <jackattack51@gmail.com>
2024-05-17 17:25:26 +0000

    Add check for update button

--- custom_components/unfoldedcircle/button.py
+++ custom_components/unfoldedcircle/button.py
@@ -17,10 +17,10 @@ async def async_setup_entry(
 ) -> None:
     """Set up entity in HA."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id][UNFOLDED_CIRCLE_COORDINATOR]
-    async_add_entities([Button(coordinator)])
+    async_add_entities([RebootButton(coordinator), UpdateCheckButton(coordinator)])
 
 
-class Button(UnfoldedCircleEntity, ButtonEntity):
+class RebootButton(UnfoldedCircleEntity, ButtonEntity):
     """Representation of a Button entity."""
 
     _attr_entity_category = EntityCategory.CONFIG
@@ -41,3 +41,29 @@ class Button(UnfoldedCircleEntity, ButtonEntity):
     async def async_press(self) -> None:
         """Press the button."""
         await self.coordinator.api.post_system_command("REBOOT")
+
+
+class UpdateCheckButton(UnfoldedCircleEntity, ButtonEntity):
+    """Representation of a Button entity."""
+
+    _attr_entity_category = EntityCategory.CONFIG
+    _attr_icon = "mdi:gesture-tap-button"
+    _attr_device_class = ButtonDeviceClass.UPDATE
+
+    def __init__(self, coordinator) -> None:
+        """Initialize the sensor."""
+        super().__init__(coordinator)
+        self._attr_unique_id = (
+            f"{self.coordinator.api.serial_number}_update_check_button"
+        )
+        self._attr_name = f"{self.coordinator.api.name} Check for Update"
+
+    @property
+    def available(self) -> bool:
+        """Return if entity is available."""
+        return self.coordinator.api.online
+
+    async def async_press(self) -> None:
+        """Press the button."""
+        await self.coordinator.api.get_remote_force_update_information()
+        self.async_write_ha_state()
