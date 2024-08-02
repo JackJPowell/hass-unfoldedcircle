747c772fdf9fa6697c2114bef90071d5253bcdbf
Jack Powell <jackattack51@gmail.com>
2024-08-02 22:10:12 +0000

    fix sensor values

--- custom_components/unfoldedcircle/sensor.py
+++ custom_components/unfoldedcircle/sensor.py
@@ -110,9 +110,13 @@ UNFOLDED_CIRCLE_SENSOR: tuple[UnfoldedCircleSensorEntityDescription, ...] = (
 # )
 
 
-async def async_setup_entry(hass: HomeAssistant, config_entry, async_add_entities):
+async def async_setup_entry(
+    hass: HomeAssistant, config_entry, async_add_entities
+):
     """Add sensors for passed config_entry in HA."""
-    coordinator = hass.data[DOMAIN][config_entry.entry_id][UNFOLDED_CIRCLE_COORDINATOR]
+    coordinator = hass.data[DOMAIN][config_entry.entry_id][
+        UNFOLDED_CIRCLE_COORDINATOR
+    ]
 
     async_add_entities(
         UnfoldedCircleSensor(coordinator, description)
@@ -174,7 +178,9 @@ class UnfoldedCircleSensor(UnfoldedCircleEntity, SensorEntity):
     def get_value(self) -> StateType:
         """return native value of entity"""
         if self.coordinator.data:
-            self._state = getattr(self.coordinator.api, self.entity_description.key)
+            self._state = getattr(
+                self.coordinator.api, self.entity_description.key
+            )
             self._attr_native_value = self._state
         return self._state
 
@@ -188,3 +194,8 @@ class UnfoldedCircleSensor(UnfoldedCircleEntity, SensorEntity):
         """Handle updated data from the coordinator."""
         self._attr_native_value = self.get_value()
         self.async_write_ha_state()
+
+    @property
+    def native_value(self) -> StateType:
+        """Return native value for entity."""
+        return self.get_value()
