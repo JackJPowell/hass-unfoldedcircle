3aef821a2af9e77823da157f09bc1c461eabbf6d
Jack Powell <jackjpowell@gmail.com>
2025-08-07 01:08:39 +0000

    Subentry support

--- custom_components/unfoldedcircle/__init__.py
+++ custom_components/unfoldedcircle/__init__.py
@@ -206,5 +206,15 @@ async def async_remove_device(hass: HomeAssistant, dock) -> None:
             )
         }
     )
+    if not device:
+        device = dev_reg.async_get_device(
+            identifiers={
+                (
+                    DOMAIN,
+                    dock.model_number,
+                    dock.serial_number,
+                )
+            }
+        )
     if device:
         dev_reg.async_remove_device(device.id)
